620a6d90ea2a11a82f35719b10d2a4df9608e086
gunwoo <gunwoo363@gmail.com>
2025-08-21 13:17:41 +0900

    0821

---
+++ llm2/open5.py
@@ -0,0 +1,18 @@
+from myLLM.MyApi import openAiModel
+
+
+def test(prompt):
+    openModel = openAiModel()
+    response = openModel.images.generate(
+        model="dall-e-3",
+        prompt="a white siamese cat and black dog",
+        size="1024x1024",
+        quality="standard",
+        n=1,
+    )
+    image_url = response.data[0].url
+    print(image_url)
+
+if __name__ == "__main__":
+    prompt = "갈색 강아지와 흰색 고양이 사진 만들어줘"
+    test(prompt)
---
+++ llm2/open6.py
@@ -0,0 +1,22 @@
+import urllib
+
+from myLLM.MyApi import openAiModel
+
+
+def test(prompt):
+    openModel = openAiModel()
+    response = openModel.images.generate(
+        model="dall-e-3",
+        prompt="a white siamese cat and black dog",
+        size="1024x1024",
+        quality="standard",
+        n=1,
+    )
+    image_url = response.data[0].url
+    print(image_url)
+    imgName = "img/dogcat.png"
+    urllib.request.urlretrieve(image_url, imgName)
+
+if __name__ == "__main__":
+    prompt = "갈색 강아지와 흰색 고양이 사진 만들어줘"
+    test(prompt)
